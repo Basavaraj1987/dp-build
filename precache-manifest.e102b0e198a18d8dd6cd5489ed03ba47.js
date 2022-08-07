2c9494a5853114cd4fae0791e57429204143c475
Chetan Naik <chetan.n@vmokshagroup.com>
2022-08-07 14:47:51 +0530

    inintial commit

---
+++ precache-manifest.e102b0e198a18d8dd6cd5489ed03ba47.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = [
+  {
+    "revision": "138bec087eaf08e036ec",
+    "url": "/static/css/main.b6ce9031.chunk.css"
+  },
+  {
+    "revision": "138bec087eaf08e036ec",
+    "url": "/static/js/main.d91f046f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7bff461f7c9f3e7774ee",
+    "url": "/static/css/2.e82d6109.chunk.css"
+  },
+  {
+    "revision": "7bff461f7c9f3e7774ee",
+    "url": "/static/js/2.257404e2.chunk.js"
+  },
+  {
+    "revision": "2703801579c52c71e481d8f2605a9bbe",
+    "url": "/static/media/shape1.27038015.svg"
+  },
+  {
+    "revision": "f044bf531a8b285e1fa239f03c3c335d",
+    "url": "/static/media/shape3.f044bf53.svg"
+  },
+  {
+    "revision": "78dc5ae61c6f2c97b51523cc2db8fc13",
+    "url": "/static/media/shape2.78dc5ae6.svg"
+  },
+  {
+    "revision": "979eb3f154283002b3fe1717f02f43ac",
+    "url": "/static/media/blog1.979eb3f1.jpg"
+  },
+  {
+    "revision": "ab3d9cae712ab45c08e2cc60721de9be",
+    "url": "/static/media/project1.ab3d9cae.jpg"
+  },
+  {
+    "revision": "13bdbd3fcafda83890288efc9109d3f3",
+    "url": "/static/media/project3.13bdbd3f.jpg"
+  },
+  {
+    "revision": "1cc655f19616c55437344591ac2f3b3d",
+    "url": "/static/media/team1.1cc655f1.jpg"
+  },
+  {
+    "revision": "851db15f9a83203dd4a82a7ef77bbf01",
+    "url": "/static/media/blog2.851db15f.jpg"
+  },
+  {
+    "revision": "a3c870d8302318510f39d8220711ede9",
+    "url": "/static/media/blog3.a3c870d8.jpg"
+  },
+  {
+    "revision": "f827a5f640b9a22060653714501ae744",
+    "url": "/static/media/project2.f827a5f6.jpg"
+  },
+  {
+    "revision": "dbc4c04ba0135b2f2f1dff6ab3289e56",
+    "url": "/static/media/team2.dbc4c04b.jpg"
+  },
+  {
+    "revision": "f1353564d7fbee53fb96a3286307c117",
+    "url": "/static/media/team3.f1353564.jpg"
+  },
+  {
+    "revision": "75f4f5b2455139d32e5a80c9a022e1c2",
+    "url": "/static/media/team4.75f4f5b2.jpg"
+  },
+  {
+    "revision": "c455117d2f8d5e2ca03f2a78265acdd9",
+    "url": "/static/media/team5.c455117d.jpg"
+  },
+  {
+    "revision": "6133fbc684bc067dfc06e223ae6c01d5",
+    "url": "/static/media/women.6133fbc6.jpg"
+  },
+  {
+    "revision": "07dcdda2d9982a0b51bebf3d44495f9d",
+    "url": "/static/media/about1.07dcdda2.jpg"
+  },
+  {
+    "revision": "01166b2edcfc1430dfcad3ec9322f240",
+    "url": "/static/media/about2.01166b2e.jpg"
+  },
+  {
+    "revision": "cac8b9d12dd10ba82391c3a698057515",
+    "url": "/static/media/client2.cac8b9d1.png"
+  },
+  {
+    "revision": "d711f69562fb0c4045a23209c9006f76",
+    "url": "/static/media/map.d711f695.png"
+  },
+  {
+    "revision": "c8deb56f5da88e44639e4fb1db4bf705",
+    "url": "/static/media/services-img2.c8deb56f.jpg"
+  },
+  {
+    "revision": "5ca48edc50f54bb5a2b0a1954ddc1ee4",
+    "url": "/static/media/blog-details.5ca48edc.jpg"
+  },
+  {
+    "revision": "20266344a71f79044d5dde05c080d39f",
+    "url": "/static/media/about2.20266344.jpeg"
+  },
+  {
+    "revision": "96f250dca6f448ccaf1f9461bec8f03b",
+    "url": "/static/media/about1.96f250dc.jpeg"
+  },
+  {
+    "revision": "a1c8a8e9b6ead9801fa5c16d536928a2",
+    "url": "/static/media/services-img1.a1c8a8e9.jpg"
+  },
+  {
+    "revision": "98d1e53aa6c4f1e4b2dd425e86742403",
+    "url": "/static/media/services-img3.98d1e53a.jpg"
+  },
+  {
+    "revision": "156fe8ca3dc5c8057aed268f988db2f9",
+    "url": "/static/media/shape-img1.156fe8ca.png"
+  },
+  {
+    "revision": "131fb04b7edd502d406eae6f6a492f1d",
+    "url": "/static/media/shape-img2.131fb04b.png"
+  },
+  {
+    "revision": "849465fc8c6b9a68c514be1d5bd24b6d",
+    "url": "/static/media/map2.849465fc.png"
+  },
+  {
+    "revision": "7c95f3279d968b373cef3aaa0488977d",
+    "url": "/static/media/footer-shape2.7c95f327.png"
+  },
+  {
+    "revision": "66779a54862a7d363f55cc84e5871269",
+    "url": "/static/media/banner-img1.66779a54.jpg"
+  },
+  {
+    "revision": "aaffd8202f39aea495a4e92910d5a18c",
+    "url": "/static/media/about3.aaffd820.jpg"
+  },
+  {
+    "revision": "7f08bb731c6ebc3973764bc2efc00eca",
+    "url": "/static/media/about4.7f08bb73.jpg"
+  },
+  {
+    "revision": "991922f6d1f8f5d35392a49a3a68d548",
+    "url": "/static/media/contact-image.991922f6.jpg"
+  },
+  {
+    "revision": "de9f366bf6672b35fe790ae94154f75a",
+    "url": "/static/media/banner-illustration.de9f366b.png"
+  },
+  {
+    "revision": "01db59358da986dad24eefaa94d6618c",
+    "url": "/static/media/about-us.01db5935.png"
+  },
+  {
+    "revision": "34193042c33e9496d0c9e1cbfff9c98e",
+    "url": "/static/media/business-people.34193042.png"
+  },
+  {
+    "revision": "870e335128838835cbc2dc2d16d2b95c",
+    "url": "/static/media/team8.870e3351.jpg"
+  },
+  {
+    "revision": "e24596583c985af3c75b6107da85c5ea",
+    "url": "/static/media/team7.e2459658.jpg"
+  },
+  {
+    "revision": "703f9107477fa5224429c8b87096286f",
+    "url": "/static/media/team6.703f9107.jpg"
+  },
+  {
+    "revision": "7610eeff4fa0f81a26a724cbd7dc2db9",
+    "url": "/static/media/banner-img2.7610eeff.jpg"
+  },
+  {
+    "revision": "0a7bfaf76bb1d91e18f9cae9f11d541b",
+    "url": "/static/media/shape5.0a7bfaf7.png"
+  },
+  {
+    "revision": "4f46c8c005a887dd21babe1a421c5a37",
+    "url": "/static/media/what-we-do-image.4f46c8c0.jpg"
+  },
+  {
+    "revision": "de245171901182349b4c3181c14c75c2",
+    "url": "/static/media/experience-image.de245171.jpg"
+  },
+  {
+    "revision": "583aae6ee13e3e467de6f0da94af921e",
+    "url": "/static/media/user1.583aae6e.jpg"
+  },
+  {
+    "revision": "181890da7459bae6c23454cf7121ed65",
+    "url": "/static/media/user2.181890da.jpg"
+  },
+  {
+    "revision": "d89e5759c382a5e0aae660cb3efcbd83",
+    "url": "/static/media/user3.d89e5759.jpg"
+  },
+  {
+    "revision": "0c26b004bf333f8adc15a397a2169c9c",
+    "url": "/static/media/project4.0c26b004.jpg"
+  },
+  {
+    "revision": "56eb3e67ee2c827f3b88a0421f848224",
+    "url": "/static/media/project5.56eb3e67.jpg"
+  },
+  {
+    "revision": "47c68aeaabd13579d338e57f5d422a0c",
+    "url": "/static/media/project6.47c68aea.jpg"
+  },
+  {
+    "revision": "ef5ef5c7ab4a45e2e1836e3c1e0f8f19",
+    "url": "/static/media/project7.ef5ef5c7.jpg"
+  },
+  {
+    "revision": "f1d798854c28c494c1f1308b149d41d9",
+    "url": "/static/media/project8.f1d79885.jpg"
+  },
+  {
+    "revision": "07257cff53bf381393e2dcf22fa73512",
+    "url": "/static/media/blog4.07257cff.jpg"
+  },
+  {
+    "revision": "30df11040ca15d47ba7ec52950ee2f9b",
+    "url": "/static/media/blog5.30df1104.jpg"
+  },
+  {
+    "revision": "8f0476deb5e15f19fc459ce4518eb89d",
+    "url": "/static/media/blog6.8f0476de.jpg"
+  },
+  {
+    "revision": "315b3a768a4fe02c3e1ce5a4f2ae9bc7",
+    "url": "/static/media/project-details.315b3a76.jpg"
+  },
+  {
+    "revision": "b1284fdb85a815ec161eb969a954822c",
+    "url": "/static/media/Flaticon.b1284fdb.eot"
+  },
+  {
+    "revision": "c78b6ecaad7e5e5fa9b5c13e8bd07a25",
+    "url": "/static/media/Flaticon.c78b6eca.woff"
+  },
+  {
+    "revision": "493f096bc43273f771ddcb1196f11882",
+    "url": "/static/media/Flaticon.493f096b.woff2"
+  },
+  {
+    "revision": "5bc0abcc78b7efab3e025335e48775f3",
+    "url": "/static/media/Flaticon.5bc0abcc.ttf"
+  },
+  {
+    "revision": "f81211d0f964b9d149d86394b8582ee4",
+    "url": "/static/media/Flaticon.f81211d0.svg"
+  },
+  {
+    "revision": "283ae8b89c0130ff07e88642055c2224",
+    "url": "/static/media/page-title-bg.283ae8b8.jpg"
+  },
+  {
+    "revision": "a649c39e43923e767ea351bc668410f6",
+    "url": "/static/media/beard-man.a649c39e.png"
+  },
+  {
+    "revision": "8c1b6aca916ec92a473a6aefc633da1e",
+    "url": "/static/media/services-shape.8c1b6aca.png"
+  },
+  {
+    "revision": "d18cabaa20693acc9113d5072fe75f21",
+    "url": "/static/media/funfacts-bg.d18cabaa.jpg"
+  },
+  {
+    "revision": "71203ce3c79e052d8a086aa2a902c5f7",
+    "url": "/static/media/home-shape.71203ce3.png"
+  },
+  {
+    "revision": "9e67a4fc9d935d6852f994491f53c745",
+    "url": "/static/media/newsletter-bg.9e67a4fc.jpg"
+  },
+  {
+    "revision": "d64272b8dfb3ac5cad95a099b66f5bd9",
+    "url": "/static/media/main-women.d64272b8.png"
+  },
+  {
+    "revision": "9df48aabec1fc83a0be727fe615e5e13",
+    "url": "/static/media/man1.9df48aab.png"
+  },
+  {
+    "revision": "c306b02ac9d9ee10f7e70154839ae924",
+    "url": "/index.html"
+  }
+];
